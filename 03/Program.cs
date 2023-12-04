8b6becad7832c9fd42681010da8142610b4e3e7e
kojisi <eve-online@yandex.ru>
2023-12-04 20:14:45 +0300

    add:3

---
+++ 03/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+3 -> Среда 
+5 -> Пятница
+*/
+Console.Write("Введите номер дня недели: ");
+int number1 = int.Parse(Console.ReadLine());
+
+switch (number1)
+{
+    case 1:
+        Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Вторник");
+        break;
+    case 3:
+        Console.WriteLine("Среда");
+        break;
+    case 4:
+        Console.WriteLine("Четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятница");
+        break;
+    case 6:
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье");
+        break;
+    default:
+        Console.WriteLine("Неправильный номер");
+        break;
+}
