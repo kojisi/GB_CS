7010a63038bb7ba8987ee0eabe35049b2977dc27
kojisi <eve-online@yandex.ru>
2023-12-04 20:37:14 +0300

    add:6

---
+++ 06/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+switch (number % 2)
+{
+    case 0:
+        Console.WriteLine($"Число {number} является чётным.");
+        break;
+    default:
+        Console.WriteLine($"Число {number} не является чётным.");
+        break;
+}
