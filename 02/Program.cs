e548bb915c62c7842771a3931d95031a2890059c
kojisi <eve-online@yandex.ru>
2023-12-04 19:59:47 +0300

    add:2

---
+++ 02/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"{number1} больше {number2}");
+    Console.WriteLine($"{number2} меньше {number1}");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine($"{number2} больше {number1}");
+    Console.WriteLine($"{number1} меньше {number2}");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
