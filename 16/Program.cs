8c1ead0956bc63b901588436b7a2017e5fbc72c2
kojisi <eve-online@yandex.ru>
2023-12-07 20:17:05 +0300

    add:16

---
+++ 16/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 16: Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+5, 25  ->  да
+-4, 16  ->  да
+25, 5  ->  да
+8,9  ->  нет
+*/
+
+Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+if (firstNumber * firstNumber == secondNumber)
+{
+    Console.Write($"{secondNumber} является квадратом {firstNumber}");
+}
+else
+{
+    Console.Write($"{secondNumber} не явлется квадратом {firstNumber}");
+}
