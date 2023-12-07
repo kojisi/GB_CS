4fb7d1b9b0ffbd56e4c1b250e56e52eb8c0ebeb8
kojisi <eve-online@yandex.ru>
2023-12-07 20:02:42 +0300

    add:13

---
+++ 13/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 99)
+{
+    if (number > 999)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+    }
+    Console.WriteLine($"Третья цифра: {number % 10}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
