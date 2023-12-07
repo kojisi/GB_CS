32d3d9fcbe332dd0993a4aaf76908e027b15bf53
kojisi <eve-online@yandex.ru>
2023-12-07 19:33:46 +0300

    add:11

---
+++ 11/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задача 11: Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+Random random = new Random();
+int threeDigitNumber = random.Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число: {threeDigitNumber}");
+
+int firstDigit = (threeDigitNumber / 100);
+int lastDigit = (threeDigitNumber % 10);
+Console.WriteLine($"Результат: {firstDigit}{lastDigit}");
